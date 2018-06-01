38b5e97d5403cd2f5b78a08ab008e3fc79191546
nifengyixiang <nifengyixiang@163.com>
2018-06-01 16:23:59 +0800

    修改相应配置，使能够打成war包部署至远程tomcat服务器

--- src/main/java/com/company/project/Application.java
+++ src/main/java/com/company/project/Application.java
@@ -2,9 +2,17 @@ package com.company.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
